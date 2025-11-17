1e3114390794b9280364041bb69880ef9eeb4f7f
Spartan1606 <143238062+Spartan1606@users.noreply.github.com>
2025-11-17 16:23:19 -0500

    Update environment.prod.ts

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://secusys-alb-1755636718.us-east-1.elb.amazonaws.com'
+  apiUrl: 'd1948tccmzzbrh.cloudfront.net' // <--- ¡Tu nueva URL aquí!
 };
